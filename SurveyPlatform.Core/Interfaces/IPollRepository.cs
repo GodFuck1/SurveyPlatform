b99f00a4dd1a9b719fb6f1ff3a2709375c0fb3e6
Ruslan Triputen <kickassdom@gmail.com>
2024-11-15 20:05:45 +0300

    Интерфейс репозитория опросов

---
+++ SurveyPlatform.Core/Interfaces/IPollRepository.cs
@@ -0,0 +1,20 @@
+﻿using SurveyPlatform.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyPlatform.Core.Interfaces
+{
+    public interface IPollRepository
+    {
+        Poll GetPollById(int id);
+        IEnumerable<Poll> GetAllPolls();
+        void CreatePoll(Poll poll);
+        void UpdatePoll(Poll poll);
+        void DeletePoll(int id);
+        void AddPollResponse(PollResponse response);
+        IEnumerable<PollResponse> GetResponsesByPollId(int pollId);
+    }
+}
