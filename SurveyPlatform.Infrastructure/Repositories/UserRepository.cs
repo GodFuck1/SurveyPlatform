38a25116a8967fea82935e5ad8f040c5948cd4a7
Ruslan Triputen <kickassdom@gmail.com>
2024-11-15 20:11:52 +0300

    Реализация интерфейса для работы с репозиторием пользователей

---
+++ SurveyPlatform.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using SurveyPlatform.Core.Entities;
+using SurveyPlatform.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyPlatform.Infrastructure.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        public void CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
