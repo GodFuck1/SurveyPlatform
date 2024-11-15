36ff9d88620b607b983a674471f9b5a23ba1068f
Ruslan Triputen <kickassdom@gmail.com>
2024-11-16 00:23:53 +0300

    Убраны старые верификации моделей

--- SurveyPlatform/Models/Requests/LoginUserRequest.cs
+++ SurveyPlatform/Models/Requests/LoginUserRequest.cs
@@ -4,10 +4,7 @@ namespace SurveyPlatform.Models.Requests
 {
     public class LoginUserRequest
     {
-        [Required(ErrorMessage = "Почта обязательна для заполнения.")]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "Пароль обязателен для заполнения.")]
         public string Password { get; set; }
     }
 }
