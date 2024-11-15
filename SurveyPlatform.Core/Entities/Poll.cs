a968070083fdae2922e2dce01ce88cd329127cc8
Ruslan Triputen <kickassdom@gmail.com>
2024-11-15 19:42:55 +0300

    Фикс времени для работоспособности postgres( поддерживает только utc)

--- SurveyPlatform.Core/Entities/Poll.cs
+++ SurveyPlatform.Core/Entities/Poll.cs
@@ -11,8 +11,8 @@ public class Poll
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public int AuthorID { get; set; }
     public User Author { get; set; }
     public List<PollOption> Options { get; set; }
