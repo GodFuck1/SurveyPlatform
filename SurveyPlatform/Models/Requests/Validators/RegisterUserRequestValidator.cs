5cc6ef90499dcc8408ab03d02d3f4bee9ec7fe38
Ruslan Triputen <kickassdom@gmail.com>
2024-11-16 00:21:17 +0300

    Новые валидаторы с использованием библиотеки FluentValidator

---
+++ SurveyPlatform/Models/Requests/Validators/RegisterUserRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SurveyPlatform.Models.Requests;
+
+namespace SurveyPlatform.API.Models.Requests.Validators
+{
+    public class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+    {
+        public RegisterUserRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Имя обязательно для заполнения.")
+                .MinimumLength(2).WithMessage("Длина имени должна быть больше 2х символов.");
+
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("Почта указана не верно.");
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Пароль обязателен для заполнения.")
+                .MinimumLength(8).WithMessage("Минимальная длина пароля - 8 символов")
+                .MaximumLength(32).WithMessage("Минимальная длина пароля - 8 символов");
+        }
+    }
+}
