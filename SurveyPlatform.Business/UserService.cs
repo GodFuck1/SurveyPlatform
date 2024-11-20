7d1b76ee05936859ef0a613dd5f5cc66a04f428b
Ruslan Triputen <kickassdom@gmail.com>
2024-11-21 02:39:17 +0300

    примерная первая реализация сервиса пользователей

--- SurveyPlatform.Business/UserService.cs
+++ SurveyPlatform.Business/UserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SurveyPlatform.DAL.Entities;
+using SurveyPlatform.DAL.Interfaces;
+using SurveyPlatform.DTOs.Requests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace SurveyPlatform.Business
 {
-    internal class UserService
+    public class UserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _userRepository.GetUserById(id);
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers();
+        }
+
+        public async Task CreateUserAsync(RegisterUserRequest user)
+        {
+
+            //_userRepository.CreateUser(user);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _userRepository.UpdateUser(user);
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            _userRepository.DeleteUser(id);
+        }
     }
 }
