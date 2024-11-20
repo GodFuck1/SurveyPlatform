6d6adb1ec7802698c7f5c08e8f8702e7f83b249e
Ruslan Triputen <kickassdom@gmail.com>
2024-11-21 02:38:17 +0300

    добавление аутентификации в проект с использованием JWT

--- SurveyPlatform/Program.cs
+++ SurveyPlatform/Program.cs
@@ -3,10 +3,13 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using SurveyPlatform.API.Models.Requests.Validators;
+using SurveyPlatform.DTOs.Requests.Validators;
 using SurveyPlatform.DAL.Interfaces;
 using SurveyPlatform.DAL.Data;
 using SurveyPlatform.DAL.Repositories;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using SurveyPlatform.Business.Mappings;
 
 namespace SurveyPlatform
 {
@@ -25,14 +28,28 @@ namespace SurveyPlatform
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-       options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
+            var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
             builder.Services.AddAuthentication(opt =>
             {
                 opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
             });
-           // .AddJwtBearer().
+
+            builder.Services.AddAutoMapper(typeof(UserMapperProfile));
+
             builder.Services.AddScoped<IPollRepository, PollRepository>();
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             var app = builder.Build();
@@ -46,6 +63,7 @@ namespace SurveyPlatform
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
