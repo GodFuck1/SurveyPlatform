0bfdcff0f6e082b3abf6e3d513fa51f3e7329293
Ruslan Triputen <kickassdom@gmail.com>
2024-11-21 04:49:05 +0300

    убрана захардкоженная роль админа

--- SurveyPlatform.Infrastructure/Entities/User.cs
+++ SurveyPlatform.Infrastructure/Entities/User.cs
@@ -12,7 +12,7 @@ namespace SurveyPlatform.DAL.Entities
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public ICollection<string> Roles { get; set; } = new List<string>{ "User","Admin" };
+        public ICollection<string> Roles { get; set; } = new List<string>{ "User" };
         public ICollection<PollResponse>? Responses { get; set; }
         public ICollection<Poll>? Polls { get; set; }
     }
