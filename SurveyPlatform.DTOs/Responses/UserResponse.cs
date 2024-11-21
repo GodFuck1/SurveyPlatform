13d28f56c9dc8d9f821763c8dfe13a11f74fac60
Ruslan Triputen <kickassdom@gmail.com>
2024-11-21 03:49:51 +0300

    Для пользователя добавлен список ролей

--- SurveyPlatform.DTOs/Responses/UserResponse.cs
+++ SurveyPlatform.DTOs/Responses/UserResponse.cs
@@ -7,6 +7,7 @@ namespace SurveyPlatform.DTOs.Responses
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public ICollection<string> Roles { get; set; }
         public ICollection<PollResponse>? Surveys { get; set; }
         public ICollection<Poll>? Polls { get; set; }
     }
