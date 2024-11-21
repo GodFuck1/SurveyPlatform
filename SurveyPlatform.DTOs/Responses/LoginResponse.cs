ec0b0cc317d0924270e5406d8e0e12f05a12dd3a
Ruslan Triputen <kickassdom@gmail.com>
2024-11-21 03:50:32 +0300

    DTO данных о регистрации пользователя

--- SurveyPlatform.DTOs/Responses/LoginResponse.cs
+++ SurveyPlatform.DTOs/Responses/LoginResponse.cs
@@ -9,7 +9,7 @@ namespace SurveyPlatform.DTOs.Responses
     public class LoginResponse
     {
         public bool Success { get; set; }
-        public string Token { get; set; }
-        public string Message { get; set; }
+        public string? Token { get; set; }
+        public string? Message { get; set; }
     }
 }
