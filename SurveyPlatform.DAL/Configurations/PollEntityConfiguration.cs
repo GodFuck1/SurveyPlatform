ef4652800925b8bcfcabfc381fb0ca00546fe039
Malinin <kickassdom@gmail.com>
2024-11-26 04:05:12 +0300

    entities configurations has been splitted to different methods

---
+++ SurveyPlatform.DAL/Configurations/PollEntityConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyPlatform.DAL.Entities;
+
+namespace SurveyPlatform.DAL.Configurations;
+
+internal static class PollEntityConfiguration
+{
+    internal static void ConfigurePollEntity(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Poll>()
+            .HasMany(p => p.Options)
+            .WithOne(o => o.Poll)
+            .HasForeignKey(o => o.PollId)
+            .HasPrincipalKey(p => p.Id);
+
+        modelBuilder.Entity<Poll>()
+            .HasMany(p => p.Responses)
+            .WithOne(r => r.Poll)
+            .HasForeignKey(r => r.PollId)
+            .HasPrincipalKey(p => p.Id);
+
+        modelBuilder.Entity<Poll>()
+            .HasOne(p => p.Author)
+            .WithMany(u => u.Polls)
+            .HasForeignKey(p => p.AuthorID)
+            .HasPrincipalKey(p => p.Id);
+
+        modelBuilder.Entity<Poll>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired();
+            entity.Property(e => e.Description).IsRequired();
+        });
+    }
+}
